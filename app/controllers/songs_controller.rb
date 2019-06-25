9cbe370901ba8981f4845c11cf263419d0e91b30
Natasha Chernis <natashachernis@gmail.com>
2019-06-25 19:18:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,6 +35,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.save
 
+    flash[:message] = "Successfully updated song."
     redirect "songs/#{@song.slug}"
   end
 
